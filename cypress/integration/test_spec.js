a9e7077de80ac1d4407fc5214bcfe84c13c6df7a
Tim Arney <tim@line37.com>
2017-01-10 10:02:58 -0500

    test locale

--- cypress/integration/test_spec.js
+++ cypress/integration/test_spec.js
@@ -2,7 +2,7 @@ describe('Initial Test', function(){
   it('cy.should - assert that <title> is correct', function(){
 
     // https://on.cypress.io/api/visit
-    cy.visit('//localhost:3000')
+    cy.visit('//127.0.0.1:3000')
     cy.title().should('include', 'Kitchen Sink')
   })
 
