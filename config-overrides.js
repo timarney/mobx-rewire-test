2a176b2c551d65c59eb9e00dc6b48df3ede59994
Tim Arney <tim@line37.com>
2017-05-19 08:38:41 -0400

    updated react-scripts

--- config-overrides.js
+++ config-overrides.js
@@ -1,7 +1,13 @@
 const rewireMobX = require('react-app-rewire-mobx');
+const rewirePreact = require('react-app-rewire-preact');
 
 /* config-overrides.js */
 module.exports = function override(config, env) {
+  if (env === "production") {
+    console.log("⚡ Production build with Preact");
+    config = rewirePreact(config, env);
+  }
+
   config = rewireMobX(config, env);
   return config;
 }
