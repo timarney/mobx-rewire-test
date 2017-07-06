c64778336ff1421bace7be075889722991261e16
Tim Arney <tim@line37.com>
2017-07-06 13:33:13 -0400

    use MobX inject

--- src/App.js
+++ src/App.js
@@ -1,39 +1,63 @@
-import React, { Component } from 'react';
-import {observable} from 'mobx';
-import {observer} from 'mobx-react';
-
-import styled from 'emotion/react';
+import React, { Component } from "react";
+import { observable, action } from "mobx";
+import { Provider, inject, observer } from "mobx-react";
+import styled from "emotion/react";
 
 const Wrapper = styled.div`
-  margin:20px;
-  border:1px solid #ff0000;
-  padding:10px;
+  margin: 20px;
+  border: 1px solid #ff0000;
+  padding: 10px;
 `;
 
+class Store {
+  @observable timer = 10;
 
-var appState = observable({
-    timer: 0
-});
+  @action
+  start() {
+    setInterval(
+      function tick() {
+        this.timer += 1;
+      }.bind(this),
+      1000
+    );
+  }
 
-appState.resetTimer = function reset() {
-    appState.timer = 0;
-};
+  @action
+  resetTimer(parent, type, id) {
+    console.log("reset");
+    this.timer = 0;
+  }
+}
 
-setInterval(function tick() {
-    appState.timer += 1;
-}, 1000);
+let the_store = new Store();
+the_store.start();
 
+@inject("store")
 @observer
+class Reset extends Component {
+  render() {
+    const { store } = this.props;
+    return (
+      <button id="reset" onClick={() => store.resetTimer()}>
+        <span className="txt">Seconds passed:</span>
+        <span className="timer">
+          {store.timer}
+        </span>
+      </button>
+    );
+  }
+}
+
 class App extends Component {
-    render() {
-        return (
-          <Wrapper>
-          <button id="reset" onClick={()=> appState.resetTimer()}>
-                <span className="txt">Seconds passed:</span> <span className="timer">{appState.timer}</span>
-          </button>
-          </Wrapper>
-         );
-    }
-};
+  render() {
+    return (
+      <Provider store={the_store}>
+        <Wrapper>
+          <Reset />
+        </Wrapper>
+      </Provider>
+    );
+  }
+}
 
 export default App;
